009cd1296639d48799e79191d71334def8950efb
Rune Rystad <rune@rystad.no>
2012-03-19 21:45:45 +0100

    Med noe FSharp

---
+++ AnagramsFinderFSharp/Module1.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.net
+
+module Module1
+
+let readLines filePath = System.IO.File.ReadAllLines filePath;
+
+let printWord (word : string) = printfn "%d";
+
+//let findAnagramsInFile filePath = 
+//    readLines filePath
+//;
+
+
---
+++ AnagramsFinderFSharp/Script.fsx
@@ -0,0 +1,21 @@
+﻿
+
+let printSeq seq1 = Seq.iter (printf "%A ") seq1; printfn ""
+
+let sortWord (word:string) = 
+    word.Trim().ToLower().ToCharArray()
+    |> Array.sort 
+    |> System.String.Concat;
+
+let printAnagrams input = 
+    input 
+    |> Seq.groupBy sortWord 
+    |> Seq.filter (fun (_, values) -> Seq.length values > 1)
+    |> Seq.iter (fun (_, values) -> printSeq values) // hvordan kan jeg få verdiene ut som en seq<seq<string>>?
+
+["word"; "drow"; "sword"; "words"; "ggg"]
+   |> printAnagrams 
+
+// Real words
+System.IO.File.ReadAllLines @"C:\Users\rune.rystad\Downloads\wordlist.txt"
+ |> printAnagrams
